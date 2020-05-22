253f45e057ada21180d81d50221935bc53a584ce
liuzhiqiang <1365022601@qq.com>
2020-05-22 17:17:50 +0800

    使用appa_real数据集微调的模型参数

---
+++ train_file/finetune_modle.py
@@ -0,0 +1,56 @@
+import better_exceptions
+from keras.applications import VGG16, ResNet50
+from keras.layers import Dense
+from keras.models import Model
+from keras import backend as K
+
+
+weight_file = "G:/Face age estimation/imdb_data/face_estimate/source_file/VGG16_Finetune_with_imdb/weights.50-6.56.hdf5"
+
+def age_mae(y_true, y_pred):
+    true_age = K.sum(y_true * K.arange(0, 101, dtype="float32"), axis=-1)
+    pred_age = K.sum(y_pred * K.arange(0, 101, dtype="float32"), axis=-1)
+    mae = K.mean(K.abs(true_age - pred_age))
+    return mae
+
+
+def get_model(model_name="VGG16"):
+    base_model = None
+
+    if model_name == "VGG16":
+        base_model = VGG16(include_top=False, weights='imagenet', input_shape=(224, 224, 3), pooling="avg")
+
+    elif model_name == "ResNet50":
+        base_model = ResNet50(include_top=False, weights='imagenet', input_shape=(224, 224, 3), pooling="avg")
+
+
+    prediction = Dense(units=101, kernel_initializer="he_normal", use_bias=False, activation="softmax",
+                       name="pred_age")(base_model.output)
+
+    model = Model(inputs=base_model.input, outputs=prediction)
+
+    model.load_weights(weight_file)
+
+
+    print('num of trainable weights before freezing the conv base:', len(model.trainable_weights))
+    set_trainable = False
+    for layer in base_model.layers:
+        if layer.name == 'block5_conv1':
+            set_trainable = True
+        if set_trainable:
+            layer.trainable = True
+        else:
+            layer.trainable = False
+
+    print('num of trainable weights after freezing the conv base:', len(model.trainable_weights))
+
+    return model
+
+
+def main():
+    model = get_model("VGG16")
+    model.summary()
+
+
+if __name__ == '__main__':
+    main()
